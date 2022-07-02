9594bb378c3cf290c838bc288ee7a127204e2229
Kyusik Shin <banaconda.shin@gmail.com>
2022-07-02 11:14:29 +0000

    add test code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/banaconda/nb-logger
+
+go 1.18
---
+++ nb-logger.go
@@ -0,0 +1,8 @@
+package nblogger
+
+import "fmt"
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi, %v. Wellcome!", name)
+	return message
+}
