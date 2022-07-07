d7e300722efd2135a64778c14d9d6fd3e8ae8254
Kyusik Shin <banaconda.shin@gmail.com>
2022-07-07 16:53:39 +0000

    Basic Logger and test

--- logger.go
+++ logger.go
@@ -3,13 +3,244 @@ package nblogger
 import (
 	"errors"
 	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"sync"
+	"time"
 )
 
-func Hello(name string) (string, error) {
-	if name == "" {
-		return "", errors.New("empty name")
+type Logger interface {
+	Trace(format string, v ...any)
+	Debug(format string, v ...any)
+	Info(format string, v ...any)
+	Warn(format string, v ...any)
+	Error(format string, v ...any)
+	SetLogLevel(level int)
+	GetLogLevel() int
+	Close()
+}
+
+const (
+	Trace = iota
+	Debug
+	Info
+	Warn
+	Error
+)
+
+const (
+	Ldate = 1 << iota
+	Ltime
+	Lmicroseconds
+	Llongfile
+	Lshortfile
+	LUTC
+	Lblocking
+	Lstdout
+	LstdFlags = Ldate | Ltime
+)
+
+const (
+	write = iota
+	exit
+)
+
+type logMessage struct {
+	cmd int
+	buf []byte
+}
+
+type BasicLogger struct {
+	level  int
+	flags  int
+	writer io.Writer
+	ch     chan logMessage
+	lock   sync.Mutex
+	wg     sync.WaitGroup
+}
+
+var levelStringMap = map[int]string{
+	Trace: "TRACE",
+	Debug: "DEBUG",
+	Info:  "INFO",
+	Warn:  "WARN",
+	Error: "ERROR",
+}
+
+func init() {
+}
+
+func NewLogger(path string, level int, bufferSize int, flags int) (Logger, error) {
+	logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, errors.New("file creation fail")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Wellcome!", name)
-	return message, nil
+	var writer io.Writer
+	if flags&Lstdout != 0 {
+		writer = io.MultiWriter(logFile, os.Stdout)
+	} else {
+		writer = io.MultiWriter(logFile)
+	}
+
+	logger := &BasicLogger{
+		level:  level,
+		flags:  flags,
+		writer: writer,
+		ch:     make(chan logMessage, bufferSize),
+		lock:   sync.Mutex{},
+		wg:     sync.WaitGroup{},
+	}
+
+	if logger.flags&Lblocking == 0 {
+		logger.wg.Add(1)
+		go logger.server()
+	}
+
+	return logger, nil
+}
+
+func (logger *BasicLogger) server() {
+	defer logger.wg.Done()
+	for {
+		if message, err := <-logger.ch; err && message.cmd == write {
+			logger.writer.Write(message.buf)
+		} else {
+			break
+		}
+	}
+}
+
+func itoa(buf *[]byte, i int, wid int) {
+	// Assemble decimal in reverse orde.
+	var b [20]byte
+	bp := len(b) - 1
+	for i >= 10 || wid > 1 {
+		wid--
+		q := i / 10
+		b[bp] = byte('0' + i - q*10)
+		bp--
+		i = q
+	}
+	// i < 10
+	b[bp] = byte('0' + i)
+	*buf = append(*buf, b[bp:]...)
+}
+
+func (logger *BasicLogger) formatHeader(buf *[]byte, level int, t time.Time, file string, line int) {
+	*buf = append(*buf, levelStringMap[level]...)
+
+	if logger.flags&(Ldate|Ltime|Lmicroseconds) != 0 {
+		if logger.flags&LUTC != 0 {
+			t = t.UTC()
+		}
+		if logger.flags&Ldate != 0 {
+			year, month, day := t.Date()
+			itoa(buf, year, 4)
+			*buf = append(*buf, '/')
+			itoa(buf, int(month), 2)
+			*buf = append(*buf, '/')
+			itoa(buf, day, 2)
+			*buf = append(*buf, ' ')
+		}
+		if logger.flags&(Ltime|Lmicroseconds) != 0 {
+			hour, min, sec := t.Clock()
+			itoa(buf, hour, 2)
+			*buf = append(*buf, ':')
+			itoa(buf, min, 2)
+			*buf = append(*buf, ':')
+			itoa(buf, sec, 2)
+			if logger.flags&Lmicroseconds != 0 {
+				*buf = append(*buf, '.')
+				itoa(buf, t.Nanosecond()/1e3, 6)
+			}
+			*buf = append(*buf, ' ')
+		}
+	}
+	if logger.flags&(Lshortfile|Llongfile) != 0 {
+		if logger.flags&Lshortfile != 0 {
+			short := file
+			for i := len(file) - 1; i > 0; i-- {
+				if file[i] == '/' {
+					short = file[i+1:]
+					break
+				}
+			}
+			file = short
+		}
+		*buf = append(*buf, file...)
+		*buf = append(*buf, ':')
+		itoa(buf, line, -1)
+		*buf = append(*buf, ": "...)
+	}
+}
+
+func (logger *BasicLogger) logging(level int, format string, v ...any) {
+	if logger.level < level {
+		return
+	}
+
+	now := time.Now()
+	logger.lock.Lock()
+	defer logger.lock.Unlock()
+	var file string
+	var line int
+
+	if logger.flags&(Lshortfile|Llongfile) != 0 {
+		logger.lock.Unlock()
+		var ok bool
+		_, file, line, ok = runtime.Caller(2)
+		if !ok {
+			file = "unknown"
+			line = 0
+		}
+		logger.lock.Lock()
+	}
+
+	s := fmt.Sprintf(format, v...)
+	var buf []byte
+	logger.formatHeader(&buf, level, now, file, line)
+	buf = append(buf, s...)
+	if len(s) == 0 || s[len(s)-1] != '\n' {
+		buf = append(buf, '\n')
+	}
+
+	if logger.flags&Lblocking == 0 {
+		message := logMessage{
+			cmd: write,
+			buf: buf,
+		}
+
+		logger.ch <- message
+	} else {
+		logger.writer.Write(buf)
+	}
+}
+
+func (logger *BasicLogger) Trace(format string, v ...any) {
+	logger.logging(Trace, format, v...)
+}
+func (logger *BasicLogger) Debug(format string, v ...any) {
+	logger.logging(Debug, format, v...)
+}
+func (logger *BasicLogger) Info(format string, v ...any) {
+	logger.logging(Info, format, v...)
+}
+func (logger *BasicLogger) Warn(format string, v ...any) {
+	logger.logging(Warn, format, v...)
+}
+func (logger *BasicLogger) Error(format string, v ...any) {
+	logger.logging(Error, format, v...)
+}
+func (logger *BasicLogger) SetLogLevel(level int) {
+	logger.level = level
+}
+func (logger *BasicLogger) GetLogLevel() int {
+	return logger.level
+}
+
+func (logger *BasicLogger) Close() {
+	logger.ch <- logMessage{cmd: exit}
+	logger.wg.Wait()
 }
--- logger_test.go
+++ logger_test.go
@@ -1,23 +1,202 @@
 package nblogger
 
 import (
-	"regexp"
+	"fmt"
+	"os"
+	"runtime"
+	"sync"
 	"testing"
+	"time"
+
+	"golang.org/x/exp/constraints"
 )
 
-func TestHello(t *testing.T) {
-	name := "Test"
-	want := regexp.MustCompile(`\b` + name + `\b`)
-	msg, err := Hello("Test")
+var LogFilePath = "/mnt/test.log"
+var bufferSize = 100000
 
-	if !want.MatchString(msg) || err != nil {
-		t.Fatalf(`Hello("Test") = %q, %v, want match for %#q, nil`, msg, err, want)
-	}
+func init() {
+	runtime.GOMAXPROCS(16)
 }
 
-func TestEmpty(t *testing.T) {
-	msg, err := Hello("")
-	if msg != "" || err == nil {
-		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+func minMaxAvg[T constraints.Integer](array []T) (T, T, T) {
+	var max T = array[0]
+	var min T = array[0]
+	var avg T = 0
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+
+		avg += value
 	}
+
+	avg /= T(len(array))
+	return min, max, avg
+}
+
+func benchmark(logger Logger) int64 {
+	var buf []byte
+	for i := 0; i < 1000; i++ {
+		buf = append(buf, "#"...)
+	}
+
+	s := string(buf)
+
+	start := time.Now()
+	for i := 0; i < bufferSize; i++ {
+		logger.Info(s)
+	}
+
+	return time.Since(start).Milliseconds()
+}
+
+func TestAsyncBenchmark(t *testing.T) {
+	logger, err := NewLogger(LogFilePath, Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds)
+	defer func() {
+		logger.Close()
+	}()
+
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	os.Remove(LogFilePath)
+	fmt.Printf("Async result: %d milis\n", benchmark(logger))
+}
+
+func TestSyncBenchmark(t *testing.T) {
+	logger, err := NewLogger(LogFilePath, Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lblocking)
+	defer func() {
+		logger.Close()
+	}()
+
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	os.Remove(LogFilePath)
+	fmt.Printf("Sync result: %d milis\n", benchmark(logger))
+}
+
+func TestAsyncThreadedBenchmark(t *testing.T) {
+	num := 20
+	wg := sync.WaitGroup{}
+	wg.Add(num)
+
+	result := make([]int64, num)
+	for i := 0; i < num; i++ {
+		logger, err := NewLogger(fmt.Sprintf("%s.%d", LogFilePath, i), Info, bufferSize, LstdFlags|Lmicroseconds)
+		defer func() {
+			logger.Close()
+		}()
+
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		go func(index int) {
+			result[index] = benchmark(logger)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	for i := 0; i < num; i++ {
+		os.Remove(fmt.Sprintf("%s.%d", LogFilePath, i))
+	}
+
+	min, max, avg := minMaxAvg(result)
+	fmt.Printf("AsyncThreaded min=%d, max=%d, avg=%d\n", min, max, avg)
+}
+
+func TestSyncThreadedBenchmark(t *testing.T) {
+	num := 20
+	wg := sync.WaitGroup{}
+	wg.Add(num)
+
+	result := make([]int64, num)
+	for i := 0; i < num; i++ {
+		logger, err := NewLogger(fmt.Sprintf("%s.%d", LogFilePath, i), Info, bufferSize, LstdFlags|Lmicroseconds|Lblocking)
+		defer func() {
+			logger.Close()
+		}()
+
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		go func(index int) {
+			result[index] = benchmark(logger)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	for i := 0; i < num; i++ {
+		os.Remove(fmt.Sprintf("%s.%d", LogFilePath, i))
+	}
+
+	min, max, avg := minMaxAvg(result)
+	fmt.Printf("SyncThreaded min=%d, max=%d, avg=%d\n", min, max, avg)
+}
+
+func TestAsyncThreadedWithRuntimeBenchmark(t *testing.T) {
+	num := 20
+	wg := sync.WaitGroup{}
+	wg.Add(num)
+
+	result := make([]int64, num)
+	for i := 0; i < num; i++ {
+		logger, err := NewLogger(fmt.Sprintf("%s.%d", LogFilePath, i), Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds)
+		defer func() {
+			logger.Close()
+		}()
+
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		go func(index int) {
+			result[index] = benchmark(logger)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	for i := 0; i < num; i++ {
+		os.Remove(fmt.Sprintf("%s.%d", LogFilePath, i))
+	}
+
+	min, max, avg := minMaxAvg(result)
+	fmt.Printf("AsyncThreadedWithRuntime min=%d, max=%d, avg=%d\n", min, max, avg)
+}
+
+func TestSyncThreadedWithRuntimeBenchmark(t *testing.T) {
+	num := 20
+	wg := sync.WaitGroup{}
+	wg.Add(num)
+
+	result := make([]int64, num)
+	for i := 0; i < num; i++ {
+		logger, err := NewLogger(fmt.Sprintf("%s.%d", LogFilePath, i), Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lblocking)
+		defer func() {
+			logger.Close()
+		}()
+
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		go func(index int) {
+			result[index] = benchmark(logger)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	for i := 0; i < num; i++ {
+		os.Remove(fmt.Sprintf("%s.%d", LogFilePath, i))
+	}
+
+	min, max, avg := minMaxAvg(result)
+	fmt.Printf("SyncThreadedWithRuntime min=%d, max=%d, avg=%d\n", min, max, avg)
 }
