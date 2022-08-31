6e0e24da7017aee835f8f7592b29b61a955c425d
Kyusik Shin <banaconda.shin@gmail.com>
2022-08-31 10:49:56 +0000

    improve performance and add MIT license
    - add license
    - change test
    - apply zenq

--- logger_test.go
+++ logger_test.go
@@ -2,6 +2,7 @@ package nblogger
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"runtime"
 	"sync"
@@ -11,14 +12,15 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-var LogFilePath = "test.log"
-var bufferSize = 100000
+var logFilePath = "test.log"
+var bufferSize = 10000
+var threadNum = 8
 
 func init() {
-	runtime.GOMAXPROCS(16)
+	runtime.GOMAXPROCS(threadNum)
 }
 
-func logging(logger Logger){
+func logging(logger Logger) {
 	logger.Trace("")
 	logger.Debug("")
 	logger.Info("")
@@ -28,41 +30,41 @@ func logging(logger Logger){
 
 func TestLogLevelDefault(t *testing.T) {
 	fmt.Printf("Info Level Test\n")
-	logger, err := NewLogger(LogFilePath, Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lstdout)
-	if err != nil{
+	logger, err := NewLogger(logFilePath, Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lstdout)
+	if err != nil {
 		t.Fatalf("%v", err)
 	}
 	defer logger.Close()
 
 	logging(logger)
 
-	os.Remove(LogFilePath)
+	os.Remove(logFilePath)
 }
 
 func TestLogLevelTrace(t *testing.T) {
 	fmt.Printf("Trace Level Test\n")
-	logger, err := NewLogger(LogFilePath, Trace, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lstdout)
-	if err != nil{
+	logger, err := NewLogger(logFilePath, Trace, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lstdout)
+	if err != nil {
 		t.Fatalf("%v", err)
 	}
 	defer logger.Close()
 
 	logging(logger)
 
-	os.Remove(LogFilePath)
+	os.Remove(logFilePath)
 }
 
 func TestLogLevelError(t *testing.T) {
 	fmt.Printf("Error Level Test\n")
-	logger, err := NewLogger(LogFilePath, Error, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lstdout)
-	if err != nil{
+	logger, err := NewLogger(logFilePath, Error, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lstdout)
+	if err != nil {
 		t.Fatalf("%v", err)
 	}
 	defer logger.Close()
 
 	logging(logger)
 
-	os.Remove(LogFilePath)
+	os.Remove(logFilePath)
 }
 
 func minMaxAvg[T constraints.Integer](array []T) (T, T, T) {
@@ -86,22 +88,24 @@ func minMaxAvg[T constraints.Integer](array []T) (T, T, T) {
 
 func benchmark(logger Logger) int64 {
 	var buf []byte
-	for i := 0; i < 1000; i++ {
-		buf = append(buf, "#"...)
+	var list []any
+	for i := 0; i < 3000; i++ {
+		buf = append(buf, "%d"...)
+		list = append(list, i)
 	}
 
 	s := string(buf)
 
 	start := time.Now()
 	for i := 0; i < bufferSize; i++ {
-		logger.Info(s)
+		logger.Info(s, list...)
 	}
 
 	return time.Since(start).Milliseconds()
 }
 
 func TestAsyncBenchmark(t *testing.T) {
-	logger, err := NewLogger(LogFilePath, Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds)
+	logger, err := NewLogger(logFilePath, Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds)
 	defer func() {
 		logger.Close()
 	}()
@@ -110,12 +114,14 @@ func TestAsyncBenchmark(t *testing.T) {
 		t.Fatalf("%v", err)
 	}
 
-	os.Remove(LogFilePath)
-	fmt.Printf("Async result: %d milis\n", benchmark(logger))
+	os.Remove(logFilePath)
+	result := benchmark(logger)
+	logger.Close()
+	log.Printf("Async result: %d milis\n", result)
 }
 
 func TestSyncBenchmark(t *testing.T) {
-	logger, err := NewLogger(LogFilePath, Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lblocking)
+	logger, err := NewLogger(logFilePath, Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lblocking)
 	defer func() {
 		logger.Close()
 	}()
@@ -124,126 +130,116 @@ func TestSyncBenchmark(t *testing.T) {
 		t.Fatalf("%v", err)
 	}
 
-	os.Remove(LogFilePath)
-	fmt.Printf("Sync result: %d milis\n", benchmark(logger))
+	os.Remove(logFilePath)
+	result := benchmark(logger)
+	logger.Close()
+	log.Printf("Sync result: %d milis\n", result)
 }
 
 func TestAsyncThreadedBenchmark(t *testing.T) {
-	num := 20
 	wg := sync.WaitGroup{}
-	wg.Add(num)
+	wg.Add(threadNum)
 
-	result := make([]int64, num)
-	for i := 0; i < num; i++ {
-		logger, err := NewLogger(fmt.Sprintf("%s.%d", LogFilePath, i), Info, bufferSize, LstdFlags|Lmicroseconds)
-		defer func() {
-			logger.Close()
-		}()
-
+	result := make([]int64, threadNum)
+	for i := 0; i < threadNum; i++ {
+		logger, err := NewLogger(fmt.Sprintf("%s.%d", logFilePath, i), Info, bufferSize, LstdFlags|Lmicroseconds)
 		if err != nil {
 			t.Fatalf("%v", err)
 		}
+
 		go func(index int) {
 			result[index] = benchmark(logger)
+			logger.Close()
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
 
-	for i := 0; i < num; i++ {
-		os.Remove(fmt.Sprintf("%s.%d", LogFilePath, i))
+	for i := 0; i < threadNum; i++ {
+		os.Remove(fmt.Sprintf("%s.%d", logFilePath, i))
 	}
 
 	min, max, avg := minMaxAvg(result)
-	fmt.Printf("AsyncThreaded min=%d, max=%d, avg=%d\n", min, max, avg)
+	log.Printf("AsyncThreaded min=%d, max=%d, avg=%d\n", min, max, avg)
 }
 
 func TestSyncThreadedBenchmark(t *testing.T) {
-	num := 20
 	wg := sync.WaitGroup{}
-	wg.Add(num)
+	wg.Add(threadNum)
 
-	result := make([]int64, num)
-	for i := 0; i < num; i++ {
-		logger, err := NewLogger(fmt.Sprintf("%s.%d", LogFilePath, i), Info, bufferSize, LstdFlags|Lmicroseconds|Lblocking)
-		defer func() {
-			logger.Close()
-		}()
-
+	result := make([]int64, threadNum)
+	for i := 0; i < threadNum; i++ {
+		logger, err := NewLogger(fmt.Sprintf("%s.%d", logFilePath, i), Info, bufferSize, LstdFlags|Lmicroseconds|Lblocking)
 		if err != nil {
 			t.Fatalf("%v", err)
 		}
+
 		go func(index int) {
 			result[index] = benchmark(logger)
+			logger.Close()
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
 
-	for i := 0; i < num; i++ {
-		os.Remove(fmt.Sprintf("%s.%d", LogFilePath, i))
+	for i := 0; i < threadNum; i++ {
+		os.Remove(fmt.Sprintf("%s.%d", logFilePath, i))
 	}
 
 	min, max, avg := minMaxAvg(result)
-	fmt.Printf("SyncThreaded min=%d, max=%d, avg=%d\n", min, max, avg)
+	log.Printf("SyncThreaded min=%d, max=%d, avg=%d\n", min, max, avg)
 }
 
 func TestAsyncThreadedWithRuntimeBenchmark(t *testing.T) {
-	num := 20
 	wg := sync.WaitGroup{}
-	wg.Add(num)
+	wg.Add(threadNum)
 
-	result := make([]int64, num)
-	for i := 0; i < num; i++ {
-		logger, err := NewLogger(fmt.Sprintf("%s.%d", LogFilePath, i), Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds)
-		defer func() {
-			logger.Close()
-		}()
-
+	result := make([]int64, threadNum)
+	for i := 0; i < threadNum; i++ {
+		logger, err := NewLogger(fmt.Sprintf("%s.%d", logFilePath, i), Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds)
 		if err != nil {
 			t.Fatalf("%v", err)
 		}
+
 		go func(index int) {
 			result[index] = benchmark(logger)
+			logger.Close()
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
 
-	for i := 0; i < num; i++ {
-		os.Remove(fmt.Sprintf("%s.%d", LogFilePath, i))
+	for i := 0; i < threadNum; i++ {
+		os.Remove(fmt.Sprintf("%s.%d", logFilePath, i))
 	}
 
 	min, max, avg := minMaxAvg(result)
-	fmt.Printf("AsyncThreadedWithRuntime min=%d, max=%d, avg=%d\n", min, max, avg)
+	log.Printf("AsyncThreadedWithRuntime min=%d, max=%d, avg=%d\n", min, max, avg)
 }
 
 func TestSyncThreadedWithRuntimeBenchmark(t *testing.T) {
-	num := 20
 	wg := sync.WaitGroup{}
-	wg.Add(num)
+	wg.Add(threadNum)
 
-	result := make([]int64, num)
-	for i := 0; i < num; i++ {
-		logger, err := NewLogger(fmt.Sprintf("%s.%d", LogFilePath, i), Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lblocking)
-		defer func() {
-			logger.Close()
-		}()
-
+	result := make([]int64, threadNum)
+	for i := 0; i < threadNum; i++ {
+		logger, err := NewLogger(fmt.Sprintf("%s.%d", logFilePath, i), Info, bufferSize, Lshortfile|LstdFlags|Lmicroseconds|Lblocking)
 		if err != nil {
 			t.Fatalf("%v", err)
 		}
+
 		go func(index int) {
 			result[index] = benchmark(logger)
+			logger.Close()
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
 
-	for i := 0; i < num; i++ {
-		os.Remove(fmt.Sprintf("%s.%d", LogFilePath, i))
+	for i := 0; i < threadNum; i++ {
+		os.Remove(fmt.Sprintf("%s.%d", logFilePath, i))
 	}
 
 	min, max, avg := minMaxAvg(result)
-	fmt.Printf("SyncThreadedWithRuntime min=%d, max=%d, avg=%d\n", min, max, avg)
+	log.Printf("SyncThreadedWithRuntime min=%d, max=%d, avg=%d\n", min, max, avg)
 }
